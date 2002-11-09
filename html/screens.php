3ee8e97041da5d8d6fbe40d77f347a2281eb8ad7
Derick Rethans <derick@derickrethans.nl>
2002-11-09 22:21:23 +0000

    - Fix aligning of the screenshot
    SVN Rev: 412

--- html/screens.php
+++ html/screens.php
@@ -27,6 +27,8 @@ as you can see in this screenshot:
 <p>
 Sample output from xdebug_dump_function_trace(), it shows all functions that
 were called plus variables (in most cases):
+</p>
+<p>
 <img align="center" src="images/xdebug_ss2.jpg">
 </p>
 
