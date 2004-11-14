573b8e1336dfa677b44cce1fd270a04040fb028a
Derick Rethans <derick@derickrethans.nl>
2004-11-14 17:12:57 +0000

    - Link downloadble docs.
    SVN Rev: 1595

--- html/include/menu-docs.php
+++ html/include/menu-docs.php
@@ -16,7 +16,8 @@
 &gt; Debugger: <a href="docs-debugger.php">Usage</a>,
 <a href="docs-gdb.php">GDB Protocol</a>,
 <a href="docs-dbgp.php">DBGp Protocol</a><br />
-&gt; <a href="docs-faq.php">FAQ: Frequently Asked Questions</a>
+&gt; <a href="docs-faq.php">FAQ: Frequently Asked Questions</a><br />
+&gt; <a href="files/docs.tar.gz"Download Documentation</a>
 </div>
 <br /><br />
 
