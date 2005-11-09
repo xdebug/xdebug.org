418da3910f0a37e706bbde54ccc1425e198d51bf
Derick Rethans <derick@derickrethans.nl>
2005-11-09 09:26:53 +0000

    - Fixed typo.
    SVN Rev: 1765

--- html/docs-profiling2.php
+++ html/docs-profiling2.php
@@ -19,7 +19,7 @@ profiling information in the form of a cachegrind compatible file.  This allows
 you to use the excellent <a href='http://kcachegrind.sf.net'>KCacheGrind</a>
 tool (Linux, KDE) to analyse your profiling data. Users of the Windows
 operating system can use <a
-href="http://sourceforge.net/projects/wincachegrind.">WinCacheGrind</a>, the
+href="http://sourceforge.net/projects/wincachegrind">WinCacheGrind</a>, the
 functionality is different from KCacheGrind so the section that documents the
 use of KCacheGrind on this page doesn't apply to this program.</p>
 
