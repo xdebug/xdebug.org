74c1faf1e02c9a3e589302148a7b440bb628ce03
Derick Rethans <derick@derickrethans.nl>
2008-10-01 16:48:08 +0000

    - Fixed date-format.
    SVN Rev: 2817

--- html/index.php
+++ html/index.php
@@ -43,7 +43,7 @@ Xdebug also provides:
 <h2>Announcements</h2>
 
 <dl class="main">
-<dt class="main">[09-04-2008]</dt>
+<dt class="main">[2008-04-09]</dt>
 <dd class="main">
 <p>I just released Xdebug 2.0.3 - which features bugfixes and PHP 5.3
 compatibility. The whole change log can be found on the <a
@@ -54,7 +54,7 @@ useful to send me a postcard with their location. (Address is <a
 href="http://derickrethans.nl/who.php">here</a> at the top of the
 page). I am looking forwards to find out who you are!</p></dd>
 
-<dt class="main">[11-11-2007]</dt>
+<dt class="main">[2007-11-11]</dt>
 <dd class="main">
 <p>I just released Xdebug 2.0.2 - which features bugfixes related to the
 improved code coverage support that was introduced in 2.0.1.  The whole change
@@ -65,14 +65,14 @@ invite everybody who finds Xdebug useful to send me a postcard with their
 location. (Address is <a href="http://derickrethans.nl/who.php">here</a> at the
 top of the page). I am looking forwards to find out who you are!</p></dd>
 
-<dt class="main">[20-10-2007]</dt>
+<dt class="main">[2007-10-20]</dt>
 <dd class="main">
 <p>I just released Xdebug 2.0.1 - which features some bugfixes,
 while the major thing is the immense increase in performance of code
 coverage.  The whole change log can be found on the <a
 href="/updates.php#x_2_0_1">updates</a> page.</p>
 
-<dt class="main">[18-07-2007]</dt>
+<dt class="main">[2007-07-18]</dt>
 <dd class="main">It is finally here, Xdebug 2.0.0 has arrived! After about four
 years of work I finally found it ready to release. Have fun!
 After almost four years of work, Xdebug 2 is finally ready. With
@@ -85,7 +85,7 @@ coverage analysis</a> and much improved <a href="/docs/remote">remote debugging<
 Xdebug's <a href="/docs">documentation</a> has also
 been rewritten for more clarity.</dd>
 
-<dt class="main">[17-05-2007]</dt>
+<dt class="main">[2007-05-17]</dt>
 <dd class="main">Xdebug version 2.0.0RC4 has been released. There are two major
 changes that needs some introduction. The first one is that the
 profiler_output_name and trace_output_name settings no longer accept simple
@@ -93,17 +93,17 @@ values such as "crc32", but instead now accept different format specifiers,
 very similar to printf() and strftime() modifiers. This will show up in the
 newly organized documentation very soon as well.</dd>
 
-<dt class="main">[31-01-2007]</dt>
+<dt class="main">[2007-01-31]</dt>
 <dd class="main">Xdebug version 2.0.0RC3 has been released. This release
 fixes a number of bugs and should be the last release candidate before 2.0.0 is
 released. Now is your time to test!</dd>
 
-<dt class="main">[24-12-2006]</dt>
+<dt class="main">[2006-12-24]</dt>
 <dd class="main">Xdebug version 2.0.0RC2 has been released. This release
 features many internal clean ups and bug fixes. Some issues with the newly
 introduced layout for stack traces where also addressed.</dd>
 
-<dt class="main">[08-10-2006]</dt>
+<dt class="main">[2006-10-08]</dt>
 <dd class="main">Xdebug version 2.0.0RC1 has been released. This release
 includes some performance enhancing patches and <a
 href='/updates.php'>fixes</a> some problems in the previous beta releases. It
@@ -111,29 +111,29 @@ also features a new layout for <a
 href='http://derickrethans.nl/pimping_xdebug_stack_traces.php'>stack traces</a>
 and features much better code-coverage support.</dd>
 
-<dt class="main">[30-06-2006]</dt>
+<dt class="main">[2006-06-30]</dt>
 <dd class="main">Xdebug version 2.0.0beta6 has been released. This release
 includes some performance enhancing patches and <a
 href='/updates.php'>fixes</a> some problems in the previous beta releases.</dd>
 
-<dt class="main">[31-12-2005]</dt>
+<dt class="main">[2005-12-31]</dt>
 <dd class="main">Xdebug version 2.0.0beta5 has been released. This release
 <a href='/updates.php'>fixes</a> some problems in the previous beta releases.</dd>
 
-<dt class="main">[29-11-2004]</dt>
+<dt class="main">[2004-11-29]</dt>
 <dd class="main">Xdebug version 2.0.0beta2 has been released. This release
 <a href='/updates.php'>fixes</a> some problems in the beta1 release.</dd>
 
-<dt class="main">[15-09-2004]</dt>
+<dt class="main">[2004-09-15]</dt>
 <dd class="main">Xdebug version 2 has finally reached beta status. We celebrate
 that with the release of 2.0.0beta1. For a full changelog, see the <a
 href='/updates.php'>updates</a> page.</dd>
 
-<dt class="main">[30-06-2004]</dt>
+<dt class="main">[2004-06-30]</dt>
 <dd class="main">Xdebug version 1.3.2 has been released. This release will
 compile against the latest CVS of PHP 5 again.</dd>
 
-<dt class="main">[06-04-2004]</dt>
+<dt class="main">[2004-04-06]</dt>
 <dd class="main">Xdebug version 1.3.1 has been released. This release just
 includes some small bugfixes:
 <ul>
@@ -145,20 +145,20 @@ includes some small bugfixes:
 </ul>
 </dd>
 
-<dt class="main">[01-04-2004]</dt>
+<dt class="main">[2004-04-01]</dt>
 <dd class="main">Added archives for the <a href='/archives/xdebug-general'>Xdebug General</a>
 and <a href='/archives/xdebug-dev'>Xdebug Development</a> mailinglists.</dd>
 
-<dt class="main">[28-12-2003]</dt>
+<dt class="main">[2003-12-28]</dt>
 <dd class="main">Added the Xdebug <a href="license.php">license</a> to this
 website.</dd>
 
-<dt class="main">[26-12-2003]</dt>
+<dt class="main">[2003-12-26]</dt>
 <dd class="main">Xdebug version 1.3.0 has been released. Changes since 1.3.0rc1
 includes numerous bugfixes and the addition of file/line information to signals.
 See the full <a href="updates.php">Changelog</a> to find out what's new. </dd>
 
-<dt class="main">[09-10-2003]</dt>
+<dt class="main">[2003-10-09]</dt>
 <dd class="main">Together with Shane Caraveo from <a
 href="http://www.activestate.com">ActiveState</a> a new protocol, <a
 href="docs-dbgp.php">DBGp</a>, was developed. This will be <a
@@ -172,44 +172,44 @@ protocol is sponsored by <a href="http://www.maguma.com">Maguma</a>  and will
 also be implemented in Maguma Studio 2.
 </dd>
 
-<dt class="main">[18-09-2003]</dt>
+<dt class="main">[2003-09-18]</dt>
 <dd class="main">Xdebug version 1.3.0rc1 has been released. This new
 version's main enhancements are the increased performance and additional
 commands for remote debugging (conditional breakpoints, "full" backtrace, show
 local variables) See the full <a href="updates.php">Changelog</a> to find out
 what's new. </dd>
 <!--
-<dt class="main">[16-07-2003] - Clients</dt>
+<dt class="main">[2003-07-16] - Clients</dt>
 <dd class="main">At this moment there are three clients being written for
 Xdebug: <a href="http://xdebug.weaverslave.ws/">XdebugC/Weaverslave</a> by
 Thomas Weinert, a native MacOSX client by Bertrand Mansion and <a
 href="http://www.maguma.com/en/index.html">Maguma Studio 2</a> will feature a
 built-in client too.</dd>
 -->
-<dt class="main">[16-07-2003]</dt>
+<dt class="main">[2003-07-16]</dt>
 <dd class="main">Xdebug version 1.3.0 will have greatly improved performance over older versions.
 Changing the folding (creating a string out of a variable) is now only done when it is required:
 when being in trace mode (started from <i><a
 href='docs.php#xdebug_start_trace'>xdebug_start_trace()</a></i>, or on error. See the full <a
 href="updates.php">Changelog</a> for information on other improvements.</dd>
 
-<dt class="main">[21-04-2003]</dt>
+<dt class="main">[2003-04-21]</dt>
 <dd class="main">Xdebug version 1.2.0 has been released. This release adds one
 extra function in comparison to 1.2.0rc2, <i><a
 href='docs.php#xdebug_call_class'>xdebug_call_class()</a></i>.  See the full <a
 href="updates.php">Changelog</a> for more information. </dd>
 
-<dt class="main">[15-04-2003]</dt>
+<dt class="main">[2003-04-15]</dt>
 <dd class="main">The second release candidate of Xdebug version 1.2.0 has
 been released. This RC fixes a few bugs in RC1. See
 the full <a href="updates.php">Changelog</a> for more information. </dd>
 
-<dt class="main">[06-04-2003]</dt>
+<dt class="main">[2003-04-06]</dt>
 <dd class="main">Xdebug version 1.2.0rc1 has finally been released. This new
 version's main enhancements are the profiling support and code coverage.  See
 the full <a href="updates.php">Changelog</a> to find out what's new. </dd>
 
-<dt class="main">[16-01-2003]</dt>
+<dt class="main">[2003-01-16]</dt>
 <dd class="main">Xdebug supports Zend Engine 2! With the updated OO
 functionality in this new engine for PHP Xdebug wouldn't compile anymore, with
 this update it works again. Namespaces are not yet supported but will be
--- html/updates.php
+++ html/updates.php
@@ -19,7 +19,7 @@
 
 <span class='quote'>
 <dl>
-<dt><a name='x_2_0_3'></a>[09-04-2008] &mdash; Xdebug 2.0.3</dt>
+<dt><a name='x_2_0_3'></a>[2008-04-09] &mdash; Xdebug 2.0.3</dt>
 <dd><h3>Fixed bugs</h3></dd>
 <dd>Fixed bug #338: Crash with: xdebug.remote_handler=req.</dd>
 <dd>Fixed bug #334: Code Coverage Regressions.</dd>
@@ -32,7 +32,7 @@
 <dd>Added support for the new E_DEPRECATED.</dd>
 
 
-<dt><a name='x_2_0_2'></a>[11-11-2007] &mdash; Xdebug 2.0.2</dt>
+<dt><a name='x_2_0_2'></a>[2007-11-11] &mdash; Xdebug 2.0.2</dt>
 <dd><h3>Fixed bugs</h3></dd>
 <dd>Fixed bug #325: DBGP: "detach" stops further sessions being established from Apache.</dd>
 <dd>Fixed bug #321: Code coverage crashes on empty PHP files.</dd>
@@ -43,7 +43,7 @@
 <dd>Always set the transaction_id and command. (Related to bug #313).</dd>
 
 
-<dt><a name='x_2_0_1'></a>[20-10-2007] &mdash; Xdebug 2.0.1</dt>
+<dt><a name='x_2_0_1'></a>[2007-10-20] &mdash; Xdebug 2.0.1</dt>
 <dd><h3>Changes</h3></dd>
 <dd>Improved code coverage performance dramatically.</dd>
 <dd>PHP 5.3 compatibility (no namespaces yet though).</dd>
@@ -56,7 +56,7 @@
 <dd>Fixed a memory free error related to remote debugging conditions.  (Related to bug #297).</dd>
 
 
-<dt>[18-07-2007] &mdash; Xdebug 2.0.0</dt>
+<dt>[2007-07-18] &mdash; Xdebug 2.0.0</dt>
 <dd><h3>Changes</h3></dd>
 <dd>Put back the disabling of stack traces - apperently people were relying on this. This brings back xdebug_enable(), xdebug_disable() and xdebug_is_enabled().</dd>
 <dd>xdebug.collect_params is no longer a boolean setting. Although it worked fine, phpinfo() showed only just On or Off here.</dd>
@@ -80,7 +80,7 @@
 <dd>Added support for "breakpoint_languages".</dd>
 
 
-<dt>[17-05-2007] &mdash; Xdebug 2.0.0RC4</dt>
+<dt>[2007-05-17] &mdash; Xdebug 2.0.0RC4</dt>
 <dd><h3>Changes</h3></dd>
 <dd>Use µ seconds instead of a tenths of µ seconds to avoid confusion in profile information.</dd>
 <dd>Changed xdebug.profiler_output_name and xdebug.trace_output_name to use modifier tags:
@@ -122,7 +122,7 @@
 <dd>Stopped the use of <error> elements for exception breakpoints, as that violates the protocol.</dd>
 
 
-<dt>[31-01-2007] &mdash; Xdebug 2.0.0RC3</dt>
+<dt>[2007-01-31] &mdash; Xdebug 2.0.0RC3</dt>
 <dd><h3>Changes</h3></dd>
 <dd>Removed the bogus "xdebug.allowed_clients" setting - it was not implemented.</dd>
 <dd>Optimized used variable collection by switching to a linked list instead of a hash. This is about 30% faster, but it needed a quick conversion to hash in the case the information had to be shown to remove duplicate variable names.</dd>
@@ -134,7 +134,7 @@
 <dd>Fixed a segfault that occurred when creating printable stack traces and collect_params was turned off.</dd>
 
 
-<dt>[24-12-2006] &mdash; Xdebug 2.0.0RC2</dt>
+<dt>[2006-12-24] &mdash; Xdebug 2.0.0RC2</dt>
 <dd><h3>Added new features</h3></dd>
 <dd>Implemented the "xdebug.var_display_max_children" setting. The default is set to 128 children.</dd>
 <dd>Added types to fancy var dumping function.</dd>
@@ -172,7 +172,7 @@
 <dd>Fixed PECL bug #8989: Compile error with PHP 5 and GCC 2.95.</dd>
 
 
-<dt>[08-10-2006] &mdash; Xdebug 2.0.0RC1</dt>
+<dt>[2006-10-08] &mdash; Xdebug 2.0.0RC1</dt>
 
 <dd><h3>Added new features</h3></dd>
 <dd>Implemented FR #70: Provide optional depth on xdebug_call_* functions.</dd>
@@ -201,7 +201,7 @@
 <dd>Fixed bug #173: Coverage produces wrong coverage.</dd>
 <dd>Fixed a typo that prevented the XDEBUG_CONFIG option "profiler_enable" from working.</dd>
 
-<dt>[30-06-2006] &mdash; Xdebug 2.0.0beta6</dt>
+<dt>[2006-06-30] &mdash; Xdebug 2.0.0beta6</dt>
 
 <dd><h3>Added new features</h3></dd>
 <dd>Implemented FR #137: feature_get for general commands doesn't have a text field.</dd>
@@ -239,7 +239,7 @@
 <dd>Suppress NOP/EXT_NOP from being marked as executable code with Code Coverage.</dd>
 
 
-<dt>[31-12-2005] &mdash; Xdebug 2.0.0beta5</dt>
+<dt>[2005-12-31] &mdash; Xdebug 2.0.0beta5</dt>
 <dd><h3>Added new features</h3></dd>
 <dd>Implemented FR #161: var_dump doesn't show lengths for strings.</dd>
 <dd>Implemented FR #158: Function calls from the {main} scope always have the line number 0. </dd>
@@ -253,7 +253,7 @@
 <dd>Fixed bug #141: Remote context_get causes segfault.</dd>
 
 
-<dt>[29-11-2004]</dt>
+<dt>[2004-11-29]</dt>
 
 <dd><h3>Added new features</h3></dd>
 <dd>DBGP: Added error messages to returned errors (in most cases)</dd>
@@ -281,7 +281,7 @@
 <dd>Fixed <?php bug(68); ?>: Summary not written when script ended with "exit()".</dd>
 
 
-<dt>[15-09-2004]</dt>
+<dt>[2004-09-15]</dt>
 
 <dd><h3>Added new features</h3></dd>
 <dd>Added support for the new DBGp protocol for communicating with the debug
@@ -356,7 +356,7 @@ the previous command when pressing just enter.</dd>
 <dd>Fixed <?php bug(32); ?>: Unusual dynamic variables cause xdebug to crash.  </dd>
 
 
-<dt>[26-12-2003]</dt>
+<dt>[2003-12-26]</dt>
 
 <dd>GDB: Added file/line to signals.</dd>
 <dd>Fixed logging to adhere to the error_reporting setting.</dd>
@@ -368,7 +368,7 @@ output.</dd>
 <i><a href='docs.php#xdebug_get_function_stack'>xdebug_get_function_stack()</a></i>
 and collect_params=1.</dd>
 
-<dt>[08-11-2003]</dt>
+<dt>[2003-11-08]</dt>
 
 <dd>Added version info to handlers which show in phpinfo() output.</dd>
 <dd>GDB: Fixed bug with continuing after breakpoint where only 'cont'
@@ -386,21 +386,21 @@ output.</dd>
 <dd>Fixed reading a filename in case of an callback to a PHP function from an
 internal function (like "array_map()").</dd>
 
-<dt>[18-09-2003]</dt>
+<dt>[2003-09-18]</dt>
 <dd>Fixed bug with wrong file names for functions called from call_user_*().</dd>
 
-<dt>[30-08-2003]</dt>
+<dt>[2003-08-30]</dt>
 <dd>Added the option "dump_superglobals" to the remote debugger. If you set
 this option to 0 the "show-local" and similar commands will not return any data
 from superglobals anymore.</dd>
 
-<dt>[30-08-2003]</dt>
+<dt>[2003-08-30]</dt>
 <dd>Fixed bug <a
 href='http://bugs.xdebug.org/bug_view_page.php?bug_id=0000002'>#2</a>: "pear
 package" triggers a segfault.</dd> <dd>Fixed crash bug when a function had
 sprintf style parameters (ie.  strftime()).</dd>
 
-<dt>[16-07-2003]</dt>
+<dt>[2003-07-16]</dt>
 <dd>Added "id" attribute to &lt;var /&gt; elements in responses from the remove
 debugger when the response method is XML. This makes it possible to distinguish
 between unique elements by use of recursion for example.</dd>
@@ -408,56 +408,56 @@ between unique elements by use of recursion for example.</dd>
 <dd>Fixed a bug with "quit" that would disable the extension.</dd>
 <dd>Fixed a bug in the folding of recursive elements.</dd>
 
-<dt>[03-07-2003]</dt>
+<dt>[2003-07-03]</dt>
 <dd>Implemented an argument to the "bt" command. If this is set to "full" all
 local variables for a stack frame are showed too.</dd>
 
-<dt>[28-06-2003]</dt>
+<dt>[2003-06-28]</dt>
 <dd>Updated licence: "PHP Licence" was renamed to "Xdebug Licence",
 "The PHP Group" to "Derick Rethans" and "PHP" to "Xdebug".</dd>
 <dd>Added the <i><a href='docs.php#xdebug_time_index'>xdebug_time_index()</a></i>
 function which returns the time index since the start of the script.</dd>
 
-<dt>[23-06-2003]</dt>
+<dt>[2003-06-23]</dt>
 <dd>Updated licence: "PHP Licence" was renamed to "Xdebug Licence",
 "The PHP Group" to "Derick Rethans" and "PHP" to "Xdebug".</dd>
 <dd>Implemented the "show-breakpoints" command which shows all currently
 active breakpoints including conditions.</dd>
 
-<dt>[14-05-2003]</dt>
+<dt>[2003-05-14]</dt>
 <dd>Implemented the "show-local" command (shows all local variables in the
 current scope including all contents) and conditions for breakpoints in the
 "break" command.</dd>
 
-<dt>[27-04-2003]</dt>
+<dt>[2003-04-27]</dt>
 <dd>Added a fancy replacement function for <a
 href='docs.php#var_dump'>var_dump</a></i> which overloads the standard PHP
 function while HTML errors are enabled.</dd>
 
-<dt>[21-04-2003]</dt>
+<dt>[2003-04-21]</dt>
 <dd>Added the <i><a href='docs.php#xdebug_call_class'>xdebug_call_class</a></i>
 function as addition to <i><a
 href='docs.php#xdebug_call_function'>xdebug_call_function</a></i>.</dd>
 
-<dt>[19-04-2003]</dt>
+<dt>[2003-04-19]</dt>
 <dd>Re-implemented the <a href='docs-settings.php#max_nesting_level'>maximum
 nesting level protection</a>.</dd>
 
-<dt>[18-04-2003]</dt>
+<dt>[2003-04-18]</dt>
 <dd>Turned off <a href='docs-settings.php#collect_params'>xdebug.collect_params
 by default</a>.</dd>
 <dd>Fix problems with symbols on MacOSX.</dd>
 
-<dt>[14-04-2003]</dt>
+<dt>[2003-04-14]</dt>
 <dd>Fixed handling pathnames and files under Windows for the debugger.</dd>
 
-<dt>[10-04-2003]</dt>
+<dt>[2003-04-10]</dt>
 <dd>Fixed accessing of superglobal variables from the debugger client.</dd>
 
-<dt>[06-04-2003]</dt>
+<dt>[2003-04-06]</dt>
 <dd>Fixed a segfault that happened while dealing with include files with ZE2.</dd>
 
-<dt>[05-04-2003]</dt>
+<dt>[2003-04-05]</dt>
 <dd>Allow remote debugging to be enabled in httpd.conf and .htaccess files.</dd>
 <dd>Added code coverage, enable with <i><a
 href='docs.php#xdebug_start_code_coverage'>xdebug_start_code_coverage()</a></i>,
@@ -469,144 +469,144 @@ The latter returns an array with each element being an array containing the
 lines and the number of times a statement was run on that line, per executed
 file.</dd>
 
-<dt>[14-02-2003]</dt>
+<dt>[2003-02-14]</dt>
 <dd>Added dumping of super globals when an error occurs.</dd>
 
-<dt>[27-01-2003]</dt>
+<dt>[2003-01-27]</dt>
 <dd>Added an XML transport layer for the remote debugger.</dd>
 
-<dt>[22-01-2003]</dt>
+<dt>[2003-01-22]</dt>
 <dd>Fixed handling of 'continue' (in addition to 'cont').</dd>
 
-<dt>[14-01-2003]</dt>
+<dt>[2003-01-14]</dt>
 <dd>Updated Xdebug to support Zend Engine 2 (PHP 5.0.0-dev).</dd>
 
-<dt>[07-01-2003]</dt>
+<dt>[2003-01-07]</dt>
 <dd>Implemented the "eval" (evalutes PHP code) to the remote debugger.</dd>
 
-<dt>[05-01-2003]</dt>
+<dt>[2003-01-05]</dt>
 <dd>Enabled support for Windows again.</dd>directing profiling output to
 
-<dt>[20-12-2002]</dt>
+<dt>[2002-12-20]</dt>
 <dd>Fixed the "init" state of the debugger, from now on you can print data
 (like environment variables) from this state.</dd>
 <dd>The array returned by get_function_trace() now includes the memory
 footprint and timing information.</dd>
 
-<dt>[08-12-2002]</dt>
+<dt>[2002-12-08]</dt>
 <dd>Automatic profiling added, including redirecting profiling output to
 disk.</dd>
 
-<dt>[29-11-2002]</dt>
+<dt>[2002-11-29]</dt>
 <dd>Added a new profiling mode: Stack-Dump.</dd>
 
-<dt>[27-11-2002]</dt>
+<dt>[2002-11-27]</dt>
 <dd>Basic profiling added to Xdebug, including three new functions:
 xdebug_start_profiling() that begins profiling process, xdebug_stop_profiling()
 that ends the profiling process and xdebug_dump_function_trace() that dumps the
 profiling data.</dd>
 
-<dt>[15-11-2002]</dt>
+<dt>[2002-11-15]</dt>
 <dd>Implemented the "kill" (kills the running script) and "delete" (deletes
 an already set breakpoint) command for the remote debugger.</dd>
 
-<dt>[13-11-2002]</dt>
+<dt>[2002-11-13]</dt>
 <dd>Parameters to functions are no longer recorded by default; use the
   xdebug.collect_params=1 setting in php.ini to turn it on again.</dd>
 
-<dt>[12-11-2002]</dt>
+<dt>[2002-11-12]</dt>
 <dd>Made the xdebug server and client working under Windows.</dd>
 
-<dt>[09-11-2002]</dt>
+<dt>[2002-11-09]</dt>
 <dd>Implemented the "next" (step over) and "finish" (step out) commands
 for the remote debugger.</dd>
 
-<dt>[07-11-2002]</dt>
+<dt>[2002-11-07]</dt>
 <dd>Lots of small bugfixes, under them memory leaks and crash bugs.</dd>
 
-<dt>[03-11-2002]</dt>
+<dt>[2002-11-03]</dt>
 <dd>Implemented the "list" (source listing), "print" (printing variable
 contents), "show" (show all variables in the scope), "step" (step through
 execution) and "pwd" (print working directory) commands to the remote
 debugger.</dd>
 
-<dt>[29-10-2002]</dt>
+<dt>[2002-10-29]</dt>
 <dd>Implemented class::method, object-&gt;method and file.ext:line style
   breakpoints.</dd>
 <dd>Changed debugger port from 7869 to 17869.</dd>
 
-<dt>[28-10-2002]</dt>
+<dt>[2002-10-28]</dt>
 <dd>Added xdebug.collect_params setting. If this setting is on (the default)
   then Xdebug collects all parameters passed to functions, otherwise they
   are not collected at all.</dd>
 <dd>Implemented correct handling of include/require and eval.</dd>
 
-<dt>[22-10-2002]</dt>
+<dt>[2002-10-22]</dt>
 <dd>Fixed bug which caused wrong filenames to be reported.</dd>
 
-<dt>[18-10-2002]</dt>
+<dt>[2002-10-18]</dt>
 <dd>Added automatic starting of function traces (xdebug.auto_trace, defaulting to
   "off").</dd>
 <dd>Xdebug no longer supports PHP versions below PHP 4.3.0pre1.</dd>
 <dd>Added gdb compatible debugger handler with support for simple (function only)
   breakpoints.</dd>
 
-<dt>[16-09-2002]</dt>
+<dt>[2002-09-16]</dt>
 <dd>Fixed bug with argument lists.</dd>
 
-<dt>[04-09-2002]</dt>
+<dt>[2002-09-04]</dt>
 <dd>Implemented remote debugger handler abstraction.</dd>
 <dd>Added a php3 compatible debugger handler.</dd>
 
-<dt>[01-09-2002]</dt>
+<dt>[2002-09-01]</dt>
 <dd>Fixed memory footprint readings by not counting memory used by xdebug.</dd>
 
-<dt>[30-08-2002]</dt>
+<dt>[2002-08-30]</dt>
 <dd>Implemented gathering of parameters to internal functions. (Only works
 with recent PHP 4.3.0-dev versions!)</dd>
 
-<dt>[28-08-2002]</dt>
+<dt>[2002-08-28]</dt>
 <dd>Bugs, better performance and update of the xdebug_get_function_trace().</dd>
 
-<dt>[27-08-2002]</dt>
+<dt>[2002-08-27]</dt>
 <dd>Add much better routine for getting data from Zend.</dd>
 
 <dl>
-<dt>[23-06-2002]</dt>
+<dt>[2002-06-23]</dt>
 <dd>Xdebug no longer relies on PHP's output buffering mechanism. This caused
   problems with PEAR.</dd>
 
-<dt>[16-06-2002]</dt>
+<dt>[2002-06-16]</dt>
 <dd>Added PECL package.xml file to make xdebug <a href="#pecl">installable</a>
   by PEAR.</dd>
 <dd>Flush the log file after every line during function tracing to file.</dd>
 <dd>Removed the xdebug_* functions from the returned/showed function stracks
   and traces.</dd>
 
-<dt>[09-06-2002]</dt>
+<dt>[2002-06-09]</dt>
 <dd>Rewrote xdebug_get_function_stack() and xdebug_get_function_trace() to return
   data in multidimensional arrays.</dd>
 
-<dt>[06-06-2002]</dt>
+<dt>[2002-06-06]</dt>
 <dd>Add support for classnames, variable include files and variable function
   names.</dd>
 <dd>Implemented links to the PHP Manual in traces.</dd>
 <dd>Added timestamps and memory usage to function traces</dd>
 
-<dt>[02-06-2002]</dt>
+<dt>[2002-06-02]</dt>
 <dd>Fixed a crash when using user defined session handlers.</dd>
 
-<dt>[29-05-2002]</dt>
+<dt>[2002-05-29]</dt>
 <dd>Implemented variable function names.</dd>
 
-<dt>[26-05-2002]</dt>
+<dt>[2002-05-26]</dt>
 <dd>Unify showing unknown data.</dd>
 
-<dt>[25-05-2002]</dt>
+<dt>[2002-05-25]</dt>
 <dd>Implemented much better parameter tracing for user defined
   functions.</dd>
 
-<dt>[18-05-2002]</dt>
+<dt>[2002-05-18]</dt>
 <dd>Renamed xdebug_get_function_trace() to xdebug_dump_function_trace().</dd>
 <dd>Implemented new xdebug_get_function_trace() to return the function trace in
   an array.</dd>
@@ -614,36 +614,36 @@ with recent PHP 4.3.0-dev versions!)</dd>
   xdebug will dump a function trace to the filename which this parameter
   speficies.Fixed functions as parameters to functions</dd>
 
-<dt>[17-05-2002]</dt>
+<dt>[2002-05-17]</dt>
 <dd>Fixed functions as parameters to functions</dd>
 
-<dt>[15-05-2002]</dt>
+<dt>[2002-05-15]</dt>
 <dd>Fix logging to the systemlog</dd>
 
-<dt>[14-05-2002]</dt>
+<dt>[2002-05-14]</dt>
 <dd>Make xdebug respect the value of the error_reporting setting</dd>
 
-<dt>[12-05-2002]</dt>
+<dt>[2002-05-12]</dt>
 <dd>Added handling single-dimensional constant arrays as parameter to functions</dd>
 
-<dt>[11-05-2002]</dt>
+<dt>[2002-05-11]</dt>
 <dd>Implemented function traces (xdebug_start_trace(), xdebug_stop_trace() and
   xdebug_get_function_trace())</dd>
 <dd>Added support for Windows to xdebug</dd>
 
-<dt>[08-05-2002]</dt>
+<dt>[2002-05-08]</dt>
 <dd>Implemented handling of static method calls (foo::bar())</dd>
 
-<dt>[07-05-2002]</dt>
+<dt>[2002-05-07]</dt>
 <dd>Implemented correct handling of include(), require() and eval()</dd>
 
-<dt>[06-05-2002]</dt>
+<dt>[2002-05-06]</dt>
 <dd>Fix removal of elements from the stack</dd>
 
-<dt>[03-05-2002]</dt>
+<dt>[2002-05-03]</dt>
 <dd>Added ini_settings and functions to disable and enable showing stacktraces from within your PHP script</dd>
 
-<dt>[02-05-2002]</dt>
+<dt>[2002-05-02]</dt>
 <dd>Added xdebug_memory_usage() which returns the amount of memory used by the PHP script</dd>
 </dl>
 
