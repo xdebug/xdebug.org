6a9049f0ada1519312b205ac248a4c86d57f86a7
Derick Rethans <derick@derickrethans.nl>
2010-03-17 17:52:02 +0000

    - Updated link to firefox extension.
    SVN Rev: 3236

--- html/docs/include/features.php
+++ html/docs/include/features.php
@@ -1171,7 +1171,7 @@ to make a connection to the debugclient.</li>
 There is also a FireFox 2 extension that allows you to trigger Xdebug\'s
 debugger or profiler without having to use XDEBUG_SESSION_START as parameter.
 You can configure which [CFG:idekey:IDE key] it uses. You can find this
-extension <a href="https://addons.mozilla.org/en-US/firefox/addon/3960">here</a>.
+extension <a href="https://addons.mozilla.org/en-US/firefox/addon/58688/">here</a>.
 </p>
 
 
