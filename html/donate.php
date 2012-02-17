306ad0e49d651cdf1d76960e0965c893a1ae4ca6
Derick Rethans <github@derickrethans.nl>
2012-02-17 13:53:33 +0000

    Bye bye fucking PayPal.

--- html/donate.php
+++ html/donate.php
@@ -19,20 +19,67 @@ the far future. It has cost
 develop, test and support Xdebug. If you find Xdebug valuable, I would
 appreciate it if you would consider a small donation.
 </p>
-<p>You can donate through PayPal by clicking on the Xdebug/donate logo:</p>
+<p>You can donate through Google Checkout by selecting an option and clicking on the "Buy Now" logo.</p>
+<p><b>Disclaimer:</b> You're not *buying* anything, you're just providing a
+donation because Xdebug saves you time!</p>
 
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_donations">
-<input type="hidden" name="business" value="paypal@xdebug.org">
-<input type="hidden" name="lc" value="GB">
-<input type="hidden" name="item_name" value="Xdebug">
-<input type="hidden" name="item_number" value="xdebug1">
-<input type="hidden" name="currency_code" value="GBP">
-<input type="hidden" name="bn" value="PP-DonationsBF:xdebug-logo-donate.png:NonHosted">
-<input type="image" src="/images/xdebug-logo-donate.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online.">
-<img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+<div align="center">
+<form action="https://checkout.google.com/api/checkout/v2/checkoutForm/Merchant/744876371445067" id="BB_BuyButtonForm" method="post" name="BB_BuyButtonForm" target="_top">
+    <table cellpadding="5" cellspacing="0" width="1%">
+        <tr>
+            <td align="right" width="1%">
+                <select name="item_selection_1">
+                    <option value="1">£5.00 - Xdebug donation (mini)</option>
+                    <option value="2">£10.00 - Xdebug donation (small)</option>
+                    <option value="3" selected="yes">£25.00 - Xdebug donation (medium)</option>
+                    <option value="4">£50.00 - Xdebug donation (large)</option>
+                    <option value="5">£100.00 - Xdebug donation (mega)</option>
+                    <option value="6">£250.00 - Xdebug donation (über-mega)</option>
+                </select>
+                <input name="item_option_name_1" type="hidden" value="Xdebug donation (mini)"/>
+                <input name="item_option_price_1" type="hidden" value="5.0"/>
+                <input name="item_option_description_1" type="hidden" value=""/>
+                <input name="item_option_quantity_1" type="hidden" value="1"/>
+                <input name="item_option_currency_1" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-1.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+                <input name="item_option_name_2" type="hidden" value="Xdebug donation (small)"/>
+                <input name="item_option_price_2" type="hidden" value="10.0"/>
+                <input name="item_option_description_2" type="hidden" value=""/>
+                <input name="item_option_quantity_2" type="hidden" value="1"/>
+                <input name="item_option_currency_2" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-2.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+                <input name="item_option_name_3" type="hidden" value="Xdebug donation (medium)"/>
+                <input name="item_option_price_3" type="hidden" value="25.0"/>
+                <input name="item_option_description_3" type="hidden" value=""/>
+                <input name="item_option_quantity_3" type="hidden" value="1"/>
+                <input name="item_option_currency_3" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-3.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+                <input name="item_option_name_4" type="hidden" value="Xdebug donation (large)"/>
+                <input name="item_option_price_4" type="hidden" value="50.0"/>
+                <input name="item_option_description_4" type="hidden" value=""/>
+                <input name="item_option_quantity_4" type="hidden" value="1"/>
+                <input name="item_option_currency_4" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-4.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+                <input name="item_option_name_5" type="hidden" value="Xdebug donation (mega)"/>
+                <input name="item_option_price_5" type="hidden" value="100.0"/>
+                <input name="item_option_description_5" type="hidden" value=""/>
+                <input name="item_option_quantity_5" type="hidden" value="1"/>
+                <input name="item_option_currency_5" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-5.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+                <input name="item_option_name_6" type="hidden" value="Xdebug donation (über-mega)"/>
+                <input name="item_option_price_6" type="hidden" value="250.0"/>
+                <input name="item_option_description_6" type="hidden" value=""/>
+                <input name="item_option_quantity_6" type="hidden" value="1"/>
+                <input name="item_option_currency_6" type="hidden" value="GBP"/>
+                <input name="shopping-cart.item-options.items.item-6.digital-content.url" type="hidden" value="http://xdebug.org/downloads.php"/>
+            </td>
+            <td align="left" width="1%">
+                <input alt="" src="https://checkout.google.com/buttons/buy.gif?merchant_id=744876371445067&amp;w=117&amp;h=48&amp;style=white&amp;variant=text&amp;loc=en_US" type="image"/>
+            </td>
+        </tr>
+    </table>
 </form>
-
+</div>
 
 <br /><br />
 
