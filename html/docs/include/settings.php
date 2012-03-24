b7a9b571771da4c41e50cff22831433fed585f80
Derick Rethans <github@derickrethans.nl>
2012-03-24 17:16:29 +0000

    Correct changes, as a 2M limit is not practical.

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -95,7 +95,8 @@ send to the client through the [FEAT:remote] feature.</p>",
 		"<p>Controls how many nested levels of array elements and object properties are
 when variables are displayed with either [FUNC:xdebug_var_dump],
 [CFG:show_local_vars] or through [FEAT:execution_trace].</p>
-<p>To disable any limitation, use <i>-1</i> as value.</p>
+<p>The maximum value you can select is 1023. You can also use <i>-1</i> as
+value to select this maximum number.</p>
 <p>This setting does not have any influence on the number of children that is
 send to the client through the [FEAT:remote] feature.</p>",
 		FUNC_STACK_TRACE | FUNC_FUNCTION_TRACE | FUNC_VAR_DUMP
