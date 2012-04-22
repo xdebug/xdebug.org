5d118a6bd6e67cf3897cc3033984289c2d230a78
Derick Rethans <github@derickrethans.nl>
2012-04-22 16:07:25 +0100

    Improve xdebug.idekey documentation.

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -286,7 +286,8 @@ to generate trace files for each request, you need to set
 The default is based on environment settings. First the environment setting
 DBGP_IDEKEY is consulted, then USER and as last USERNAME.  The default is set
 to the first environment variable that is found. If none could be found the
-setting has as default ''.",
+setting has as default ''. If this setting is set, it always overrides
+the environment variables.",
 		FUNC_REMOTE
 	),
 
