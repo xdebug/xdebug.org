bd7b6875d0843fdb863a0af3ecb1d38833787536
Derick Rethans <github@derickrethans.nl>
2014-01-04 12:12:11 +0000

    Added that this only works for 2.3 and later.

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -194,8 +194,9 @@ each of the values provides. See also the introduction of the feature
 
 	'collect_return' => array(
 		'boolean', 0, null,
-		"This setting, defaulting to 0, controls whether Xdebug should write the
-return value of function calls to the trace files.",
+		"<p>This setting, defaulting to 0, controls whether Xdebug should write the
+return value of function calls to the trace files.</p>
+<p>For computerized trace files ([CFG:trace_format]=1) this only works from Xdebug 2.3 onwards.</p>",
 		FUNC_FUNCTION_TRACE,
 	),
 
