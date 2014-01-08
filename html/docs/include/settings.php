8013128d85f5fbd38c21c5f8132f28ca6915636c
Derick Rethans <github@derickrethans.nl>
2014-01-08 22:54:59 +0000

    Document new settings for xdebug.force_error_reporting and xdebug.force_display_errors.

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -28,6 +28,26 @@ slowing down your scripts. This setting can not be set in your scripts with
 ini_set(), but only in php.ini.",
 		FUNC_REMOTE
 	),
+	'force_display_errors' => array(
+		'int', 0, "2.3",
+		"<p>If this setting is set to <code>1</code> then errors will
+		<b>always</b> be displayed, no matter what the setting of PHP's <a
+		href='http://php.net/manual/en/errorfunc.configuration.php#ini.display-errors'>display_errors</a>
+		is.</p>",
+		FUNC_BASIC
+	),
+	'force_error_reporting' => array(
+		'int', 0, "2.3",
+		"<p>This setting is a bitmask, like <a
+		href='http://php.net/manual/en/errorfunc.configuration.php#ini.error-reporting'>error_reporting</a>.
+		This bitmask will be logically ORed with the bitmask represented by <a
+		href='http://php.net/manual/en/errorfunc.configuration.php#ini.error-reporting'>error_reporting</a>
+		to dermine which errors should be displayed. This setting can only be
+		made in php.ini and allows you to force certain errors from being
+		shown no matter what an application does with <a
+		href='http://php.net/manual/en/function.ini-set.php'>ini_set()</a>.</p>",
+		FUNC_BASIC
+	),
 	'halt_level' => array(
 		'int', 0, "2.3",
 		"<p>This setting allows you to configure a mask that determines
