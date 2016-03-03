043f48f25e75a99c04aa04ea0b1abb4f95230149
Derick Rethans <github@derickrethans.nl>
2016-03-03 17:41:59 +0000

    Added documentations for the new function monitor, and changed overload_var_dump default

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -565,15 +565,18 @@ this setting to 1, otherwise leave it set to 0.",
 	),
 
 	'overload_var_dump' => array(
-		'boolean', 1, '> 2.1',
+		'boolean', 2, '> 2.1',
 		"<p>By default Xdebug overloads var_dump() with its own improved version
 		for displaying variables when the html_errors php.ini setting is set to
-		1. In case you do not want that, you can set this setting to 0, but
-		check first if it's not smarter to turn off html_errors.</p>
+		<code>1</code> or <code>2</code>. In case you do not want that, you can
+		set this setting to <code>0</code>, but check first if it's not smarter
+		to turn off html_errors.</p>
 		<p>You can also use <code>2</code> as value for this setting. Besides
 		formatting the var_dump() output nicely, it will also add filename and 
 		line number to the output. The [CFG:file_link_format] setting is also
-		respected. <i>(New in Xdebug 2.3)</i></p>",
+		respected. <i>(New in Xdebug 2.3)</i></p>
+		<p>Before Xdebug 2.4, the default value of this setting was
+		<code>1</code>.</p>",
 		FUNC_VAR_DUMP
 	),
 
