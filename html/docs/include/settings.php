bb68b7d57f6b211449126ab593e12d090d2b82d7
Derick Rethans <github@derickrethans.nl>
2016-12-16 12:36:59 +0000

    Note that Unix domain sockets is new in 2.6.

--- html/docs/include/settings.php
+++ html/docs/include/settings.php
@@ -449,9 +449,10 @@ the only supported protocol.</p>
 
 	'remote_host' => array(
 		'string', 'localhost', null,
-		"Selects the host where the debug client is running, you can either use a
+		"<p>Selects the host where the debug client is running, you can either use a
 host name, IP address, or 'unix:///path/to/sock' for a Unix domain socket.
-This setting is ignored if [CFG:remote_connect_back] is enabled.",
+This setting is ignored if [CFG:remote_connect_back] is enabled.</p>
+<p>Support for Unix domain sockets was introduced in Xdebug 2.6.</p>",
 		FUNC_REMOTE
 	),
 
