fd20e72c612847942011232240a8d50977028288
Derick Rethans <github@derickrethans.nl>
2019-06-29 15:58:41 +0100

    Fixed syntax in scanner

--- html/include/phpinfo-scanner.php
+++ html/include/phpinfo-scanner.php
@@ -2,8 +2,8 @@
 class xdebugVersion
 {
 	static public $notSupportedBefore = '2.7';
-	static public $latestVersion = '2.8.0alpha1',
-	static public $latestWindowsVersion = '2.8.0alpha1',
+	static public $latestVersion = '2.8.0alpha1';
+	static public $latestWindowsVersion = '2.8.0alpha1';
 
 	static function getVersions()
 	{
@@ -11,7 +11,7 @@ class xdebugVersion
 			'7.0' => [ 'src' => '2.7.2',                              ],
 			'7.1' => [ 'src' => '2.7.2',       'win' => '2.7.2'       ],
 			'7.2' => [ 'src' => '2.7.2',       'win' => '2.7.2'       ],
-			'7.3' => [ 'src' => '2.7.2',       'win' => '2.7.2'       ]
+			'7.3' => [ 'src' => '2.7.2',       'win' => '2.7.2'       ],
 			'7.4' => [ 'src' => '2.8.0alpha1', 'win' => '2.8.0alpha1' ]
 		];
 	}
