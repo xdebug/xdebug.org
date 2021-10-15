e3a22b80b80f20fefa34c9bb7d45e25c0fa01fe0
Derick Rethans <github@derickrethans.nl>
2021-10-15 14:33:11 +0100

    Go with 3.1.1

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -240,7 +240,7 @@ debug control flow and examine data structures.",
 
 	public static function add_keywords(string $text) : string
 	{
-		$text = str_replace( '[KW:last_release_version]', '3.1.0', $text );
+		$text = str_replace( '[KW:last_release_version]', '3.1.1', $text );
 		$text = str_replace( '[KW:last_dev_version]',     '3.2.0dev', $text );
 		return $text;
 	}
