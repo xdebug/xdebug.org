609d200784d278067545ed27504494db47c8c0ff
Derick Rethans <github@derickrethans.nl>
2024-04-30 17:56:49 +0100

    Add href to projects page

--- views/funding/index.php
+++ views/funding/index.php
@@ -6,7 +6,8 @@ XdebugDotOrg\Controller\TemplateController::setTitle('Xdebug: Projects');
 
 <div class="projects">
 	<?php foreach ($this->projects as $project) : ?>
-		<h2><?= $project->title ?></h2>
+		<?php $id = preg_replace( '@-.*$@', '', $project->id ); ?>
+		<h2> <?= $project->title ?> <a name="<?= $id ?>" href="#<?= $id ?>">#<?= $id ?></a></h2>
 		<div class='funding'>
 			<div title='Contributions' class='business' style='width: <?= ($project->amountRaised / $project->amountRequested) * 100 ?>%'></div>
 			<div class='comment'><?= sprintf( '%0.1f %% of £%.0f Raised', ($project->amountRaised / $project->amountRequested) * 100, $project->amountRequested) ?></div>
