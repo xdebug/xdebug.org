f2f56077380c9819a74944804138599706293eab
Derick Rethans <github@derickrethans.nl>
2026-03-19 14:09:05 +0000

    Update development version

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -312,7 +312,7 @@ debug control flow and examine data structures.",
 	public static function add_keywords(string $text) : string
 	{
 		$text = str_replace( '[KW:last_release_version]', '3.5.1', $text );
-		$text = str_replace( '[KW:last_dev_version]',     '3.5.0dev', $text );
+		$text = str_replace( '[KW:last_dev_version]',     '3.6.0dev', $text );
 		return $text;
 	}
 }
